caf21fa76727dc8f28b7fb5d788956cc4c34a788
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2025-12-06 10:36:11 -0300

    correção na routes private

--- server/src/routes/private/user.routes.ts
+++ server/src/routes/private/user.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { listUsersController } from "../../controller/user/list-users.controller";
 import { deleteUserController } from "../../controller/user/delete-user.controller";
 import { changeUserController } from "../../controller/user/change-user.controller";
-import { changeUserStateController } from "../../controller/change-user-state.controller";
+import { changeUserStateController } from "../../controller/user/change-user-state.controller";
 import { logoutUserController } from "../../controller/user/logout-user.controller";
 import { findUserByIdController } from "../../controller/user/find-user-by-id.controller";
 
